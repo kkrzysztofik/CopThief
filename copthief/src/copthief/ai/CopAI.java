3d427440f35bae2e40a9257372a868e67688d64b
Krzysztof Krzysztofik <kkrzysztofik@teonite.com>
2014-05-09 00:35:04 +0200

    threads example fixes

--- copthief/src/copthief/ai/CopAI.java
+++ copthief/src/copthief/ai/CopAI.java
@@ -2,6 +2,7 @@ package copthief.ai;
 
 import copthief.engine.*;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -21,15 +22,18 @@ public class CopAI extends PlayerGroup {
 
     public void run() {
         RandomSingleton rand = RandomSingleton.getInstance();
-        while (!Thread.currentThread().isInterrupted()) {
-            for (Player plr : this.players) {
-                LinkedList<Constants.Direction> moves = new LinkedList<Constants.Direction>();
-                for (int i = 0; i < k; i++) {
-                    int value = rand.nextInt(5);
-                    moves.add(Constants.Direction.fromInteger(value));
-                }
-                plr.setMoves(moves);
+//        Random rand = new Random();
+
+        Iterator<Player> iterator = this.players.iterator();
+        while (!Thread.currentThread().isInterrupted() && iterator.hasNext()) {
+            Player plr = iterator.next();
+            LinkedList<Constants.Direction> moves = new LinkedList<Constants.Direction>();
+
+            for (int i = 0; i < k; i++) {
+                int value = rand.nextInt(5);
+                moves.add(Constants.Direction.fromInteger(value));
             }
+            plr.setMoves(moves);
         }
     }
 }
--- copthief/src/copthief/ai/ThiefAI.java
+++ copthief/src/copthief/ai/ThiefAI.java
@@ -5,7 +5,9 @@ import copthief.engine.Player;
 import copthief.engine.PlayerGroup;
 import copthief.engine.RandomSingleton;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Random;
 
 public class ThiefAI extends PlayerGroup {
     public ThiefAI() {
@@ -22,15 +24,17 @@ public class ThiefAI extends PlayerGroup {
 
     public void run() {
         RandomSingleton rand = RandomSingleton.getInstance();
-        while(!Thread.currentThread().isInterrupted()) {
-            for (Player plr : this.players) {
-                LinkedList<Constants.Direction> moves = new LinkedList<Constants.Direction>();
-                for (int i = 0; i < k; i++) {
-                    int value = rand.nextInt(5);
-                    moves.add(Constants.Direction.fromInteger(value));
-                }
-                plr.setMoves(moves);
+//        Random rand = new Random();
+        Iterator<Player> iterator = this.players.iterator();
+        while (!Thread.currentThread().isInterrupted() && iterator.hasNext()) {
+            Player plr = iterator.next();
+            LinkedList<Constants.Direction> moves = new LinkedList<Constants.Direction>();
+
+            for (int i = 0; i < k; i++) {
+                int value = rand.nextInt(5);
+                moves.add(Constants.Direction.fromInteger(value));
             }
+            plr.setMoves(moves);
         }
     }
 }
